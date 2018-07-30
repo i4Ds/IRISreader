1cccc96b57bfb86b1f0f857c5f6689cd4b4e974a
Cédric Huwyler <cedric.huwyler@fhnw.ch>
2018-07-30 14:32:05 +0200

    file removed

--- irisreader/raster.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-###############################################################################
-# raster class: manages access to a series of raster files
-###############################################################################
-
-class raster():
-    
-    def __init__( self, directory, line ):
