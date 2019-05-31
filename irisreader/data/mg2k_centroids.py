ee20feeae25222e9cb856ea192ac36a88298c98d
Cédric Huwyler <cedric.huwyler@fhnw.ch>
2019-05-31 14:36:57 +0200

    added capability to specify custom centroids

--- irisreader/data/mg2k_centroids.py
+++ irisreader/data/mg2k_centroids.py
@@ -11,12 +11,9 @@ from irisreader.utils import date
 
 DATA_PATH = pkg_resources.resource_filename( 'irisreader', 'data/' )
 
-# wavelength window
+# default wavelength window
 LAMBDA_MIN = 2793.8500976562500
 LAMBDA_MAX = 2799.3239974882454
-
-# number of centroids
-N_CENTROIDS = 53
 
 def get_mg2k_centroids( bins=216 ):
     """
@@ -77,7 +74,7 @@ def get_mg2k_centroids( bins=216 ):
     return centroids
 
 
-def assign_mg2k_centroids( X ):
+def assign_mg2k_centroids( X, centroids=None ):
     """
     Assigns Mg II k centroids found in the study
     'Identifying typical Mg II flare spectra using machine learning', by
@@ -98,6 +95,9 @@ def assign_mg2k_centroids( X ):
     ----------
     X : numpy.array
         interpolated raster image of shape (_,bins)
+    centroids : numpy.array
+        If None, the centroids defined in the above study will be used, otherwise an array of shape (n_centroids, n_bins) should be passed.
+        Important: both the spectra in 'X' and in 'centroids' should be constrained to the same wavelength region!
     
     Returns
     -------
@@ -105,14 +105,16 @@ def assign_mg2k_centroids( X ):
         numpy vector with shape (X.shape[1],)
     """
 
-    # load centroids
-    bins = X.shape[1]
-    centroids = get_mg2k_centroids( bins )
+    # load default centroids if no centroids are passed
+    if centroids is None:
+        centroids = get_mg2k_centroids( bins=X.shape[1] )
+
+    # create list of numbered centroid ids        
     centroid_ids = list( range( centroids.shape[0] ) )
     
     # check whether X comes in the correct dimensions
     if not X.shape[1] == centroids.shape[1]:
-        raise ValueError( "Expecting X to have shape (_,{}). Please interpolate accordingly (More information with 'help(assign_mg2k_centroids)').".format( centroids.shape[1] ) )
+        raise Exception( "Expecting X to have shape (_,{}). Please interpolate accordingly (More information with 'help(assign_mg2k_centroids)').".format( centroids.shape[1] ) )
     
     # create nearest centroid finder instance and fit it
     knc = NearestCentroid()
@@ -137,15 +139,9 @@ def normalize( X ):
     
     return X / np.max( X, axis=1 ).reshape(-1,1)
 
-def interpolate( raster, step, bins=216 ):
+def interpolate( raster, step, bins=216, lambda_min=LAMBDA_MIN, lambda_max=LAMBDA_MAX ):
     """
-    Assigns Mg II k centroids found in the study
-    'Identifying typical Mg II flare spectra using machine learning', by
-    B. Panos et. al. 2018 to the Mg II k spectra supplied in X. The centroids
-    are assigned using a nearest neighbour procedure.
-    
-    The spectra in X have to be interpolated to 216 wavelength bins between 
-    LAMBDA_MIN = 2793.8500976562500 and LAMBDA_MAX = 2799.3239974882454. For example:
+    Returns an interpolated image step from the raster.
     
     Parameters
     ----------
@@ -155,6 +151,10 @@ def interpolate( raster, step, bins=216 ):
         image step in the raster
     bins : int
         number of bins to interpolate to (defaults to 216)
+    lambda_min : float
+        wavelength value where interpolation should start
+    lambda_max : float
+        wavelength value where interpolation should stop
         
     Returns
     -------
@@ -168,75 +168,12 @@ def interpolate( raster, step, bins=216 ):
     
     return raster.get_interpolated_image_step( 
         step = step, 
-        lambda_min = LAMBDA_MIN, 
-        lambda_max = LAMBDA_MAX, 
+        lambda_min = lambda_min, 
+        lambda_max = lambda_max, 
         n_breaks = bins 
     )
 
-def compare_plot( raster, step, i ):
-    """
-    Compares spectrum <i> of image <step> of raster <raster> visually to its assigned centroid.
-    
-    Parameters
-    ----------
-    raster : irisreader.raster_cube
-        raster_cube instance
-    step : int
-        image step in the raster
-    i : int
-        row index of the spectrum in raster image <step>
-        
-    """
-    
-    # make sure we are dealing with a Mg II - raster
-    if not "Mg II k" in raster.line_info:
-        raise ValueError("This is not a Mg II k raster!")
-    
-    centroids = get_mg2k_centroids()
-    distances = compute_distances( raster, step, i )
-    sorted_distances = sorted( distances )
-    X = normalize( interpolate( raster, step ) )
-    
-    best_centroid = np.where( distances == sorted_distances[0] )[0][0]
-    second_best_centroid = np.where( distances == sorted_distances[1] )[0][0]
-    
-    print( best_centroid, second_best_centroid )
-    
-    plt.plot( centroids[best_centroid], label="best centroid" )
-    plt.plot( centroids[second_best_centroid], label="second-best centroid" )
-    plt.plot( X[i,:], label="data" )
-    plt.legend()
-    plt.show()
-    
-def compute_distances( raster, step, i ):
-    """
-    Computes euclidean distances to all centroids for spectrum <i> of image <step> of raster <raster>
-    
-    Parameters
-    ----------
-    raster : irisreader.raster_cube
-        raster_cube instance
-    step : int
-        image step in the raster
-    i : int
-        row index of the spectrum in raster image <step>
-
-    Returns
-    -------
-    distances : np.array
-        vector with euclidean distances to each centroid
-    """        
-    
-    # make sure we are dealing with a Mg II - raster
-    if not "Mg II k" in raster.line_info:
-        raise ValueError("This is not a Mg II k raster!")
-    
-    centroids = get_mg2k_centroids()
-    x = normalize( interpolate( raster, step ) )[i,:]
-    distances = np.sqrt( np.sum( (centroids - x)**2, axis=1 ) )
-    return distances
-
-def get_mg2k_centroid_table( obs, crop_raster=False ):
+def get_mg2k_centroid_table( obs, centroids=None, lambda_min=LAMBDA_MIN, lambda_max=LAMBDA_MAX, crop_raster=False ):
     """
     Returns a data frame with centroid counts for each raster image of a given
     observation.
@@ -245,6 +182,12 @@ def get_mg2k_centroid_table( obs, crop_raster=False ):
     ----------
     obs_path : str
         Path to observation
+    centroids : numpy.array
+        if None, the centroids defined in the above study will be used, otherwise an array of shape (n_centroids, n_bins) should be passed
+    lambda_min : float
+        wavelength value where interpolation should start
+    lambda_max : float
+        wavelength value where interpolation should stop
     crop_raster : bool
         Whether to crop raster before assigning centroids. If set to False,
         spectra which are -200 everywhere will be assigned to centroid 51 and
@@ -267,6 +210,14 @@ def get_mg2k_centroid_table( obs, crop_raster=False ):
     
     if crop_raster:
         raster.crop()
+
+    # infer number of centroids and number of bins
+    if centroids is None:
+        n_centroids = 53
+        bins = 216
+    else:
+        n_centroids = centroids.shape[0]
+        bins = centroids.shape[1]
     
     # get goes flux
     try:
@@ -283,14 +234,14 @@ def get_mg2k_centroid_table( obs, crop_raster=False ):
     
     # assign centroids for each image and create aggregated data frame
     for step in range( raster.n_steps ):
-        
-        # fetch assigned centroids
-        img = interpolate( raster, step )
-        img_assigned_centroids = assign_mg2k_centroids( img )
+
+        # fetch assigned centroids            
+        img = interpolate( raster, step, bins=bins, lambda_min=lambda_min, lambda_max=lambda_max )
+        img_assigned_centroids = assign_mg2k_centroids( img, centroids=centroids )
         assigned_centroids.append( img_assigned_centroids )
         
         # count centroids
-        centroids, counts = np.unique( img_assigned_centroids, return_counts=True )
+        recovered_centroids, counts = np.unique( img_assigned_centroids, return_counts=True )
         
         # append to data frame
         df = df.append( 
@@ -299,7 +250,7 @@ def get_mg2k_centroid_table( obs, crop_raster=False ):
                          'date': date.from_Tformat( raster.headers[step]['DATE_OBS'] ), 
                          'image_no': step, 
                          'goes_flux': goes_flux[step],
-                         'centroid': centroids, 
+                         'centroid': recovered_centroids, 
                          'count': counts }
                         ),
                 sort = False
@@ -309,13 +260,14 @@ def get_mg2k_centroid_table( obs, crop_raster=False ):
     df = df.pivot_table(index=['full_obsid', 'date', 'image_no', 'goes_flux'], columns='centroid', values='count', aggfunc='first', fill_value=0 )
     df.reset_index( inplace=True )
     
+    
     # make sure all centroids are represented
-    for i in range( N_CENTROIDS ):
+    for i in range( n_centroids ):
         if i not in df:
             df[i] = 0
     
     # make sure columns are sorted
-    df = df.reindex( ["full_obsid", "date", "image_no", "goes_flux"] + [i for i in range(N_CENTROIDS)], axis=1 )
+    df = df.reindex( ["full_obsid", "date", "image_no", "goes_flux"] + [i for i in range(n_centroids)], axis=1 )
     df.columns.name = ''
     
     # rename number columns into string columns
@@ -338,3 +290,6 @@ if __name__ == "__main__":
     from irisreader import observation
     obs_path = '/home/chuwyler/Desktop/FITS/20140128_073021_3860259280/'
     d, c = get_mg2k_centroid_table( observation( obs_path ) )
+    
+    new_centroids = get_mg2k_centroids()[:10,:]
+    d, c = get_mg2k_centroid_table( observation( obs_path ), centroids=new_centroids )
