ed649233f8118f804583dd43d3fe08c2bd5dbe87
Cédric Huwyler <cedric.huwyler@fhnw.ch>
2021-04-12 11:19:06 +0200

    changed version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='irisreader',
-      version='0.3.1',
+      version='0.3.2',
       description='IRISreader is a Python library that allows for efficient browsing through IRIS satellite data in order to simplify machine learning applications',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -15,6 +15,6 @@ setup(name='irisreader',
       packages=find_packages(),
       package_data={'irisreader': ['data/*.fits', 'data/20140518_151415_3820607204/*.fits', 'data/*.npz'] },
       install_requires=['numpy', 'pandas', 'matplotlib', 'astropy', 'scipy', 'scikit-learn', 'requests', 'beautifulsoup4', 'tqdm', 'ipython'],
-      download_url='https://github.com/i4Ds/IRISreader/archive/v0.3.1.tar.gz',
+      download_url='https://github.com/i4Ds/IRISreader/archive/v0.3.2.tar.gz',
       zip_safe=False)
 
