53d25ba0027d20c0785237fe08e795d2531523df
Cédric Huwyler <cedric.huwyler@fhnw.ch>
2021-05-03 11:18:19 +0200

    implemented more efficient versions of find_closest_raster and find_closest_sji, speedup by factor of ~60 and caching (this is not downward compatible)

--- irisreader/coalignment/iris_sji_raster.py
+++ irisreader/coalignment/iris_sji_raster.py
@@ -5,41 +5,26 @@ functions for coalignment of SJI and raster data
 """
 
 import numpy as np
+import pandas as pd
+from functools import lru_cache
 
-def find_closest_sji( sji, raster, raster_step ):
+# internal function to sort steps of an observation in a dataframe
+@lru_cache(maxsize=64)
+def sort_steps( raster, sji ):
+    rts = raster.get_timestamps()
+    sts = sji.get_timestamps()
+    df = pd.DataFrame({
+        'timestamp': np.hstack([rts, sts]),
+        'origin': np.hstack([['raster']*len(rts), ['sji']*len(sts)]),
+        'step': np.hstack([np.arange(len(rts)), np.arange(len(sts))])
+    })
+    df = df.set_index('timestamp').sort_index()
+    return df
+
+@lru_cache(maxsize=64)
+def find_closest_raster( raster, sji ):
     """
-    Finds closest sji step to raster step <raster_step> in time.
-    
-    Parameters
-    ----------
-    sji : irisreader.sji_cube
-        sji_cube_instance
-    raster : irisreader.raster_cube
-        raster_cube instance
-    raster_step : int
-        raster step
-        
-    Returns
-    -------
-    sji_step : int
-        SJI step
-    diff : float
-        Difference in seconds
-    """
-    
-    # check if data cubes come from the same observation and raise exception if not
-    if raster.primary_headers['STARTOBS'] != sji.primary_headers['STARTOBS']:
-        raise Exception("data cubes are not from the same observation!")
-    
-    diff = np.array( sji.get_timestamps() )-raster.get_timestamps()[raster_step]
-    sji_step = np.argmin( np.abs( diff ) )
-    
-    return [sji_step, diff[sji_step]]
-    
-    
-def find_closest_raster( raster, sji, sji_step ):
-    """
-    Finds closest raster step to SJI step <sji_step> in time.
+    Finds closest raster steps to sji steps for a given raster and sji object.
     
     Parameters
     ----------
@@ -47,23 +32,53 @@ def find_closest_raster( raster, sji, sji_step ):
         raster_cube instance
     sji : irisreader.sji_cube
         sji_cube_instance
-    sji_step : int
-        sji step
+
+    Returns
+    -------
+    raster_steps : numpy.array
+        array with closest raster steps
+    """
+
+    
+    # sort steps
+    df = sort_steps( raster, sji )
+    
+    # get available sji steps
+    raster_steps = []
+    
+    for sji_step in range(sji.n_steps):
+    
+        # timestamp of sji
+        sji_tstamp = df[(df.origin=='sji') & (df.step==sji_step)].index.values[0]
+
+        # get closest rasters on both sides
+        below = df[(df.origin=='raster') & (df.index<=sji_tstamp)].tail(1)
+        above = df[(df.origin=='raster') & (df.index>sji_tstamp)].head(1)
+        candidates = pd.concat( [below, above], axis=0 )
+        
+        # append closest raster step of both
+        raster_steps.append( candidates.step.values[np.argmin(np.abs(candidates.index - sji_tstamp))] )
+     
+    return np.array(raster_steps)
+
+
+@lru_cache(maxsize=64)
+def find_closest_sji( raster, sji ):
+    """
+    Finds closest sji steps to raster steps for a given sji and raster object.
+    
+    Parameters
+    ----------
+    raster : irisreader.raster_cube
+        raster_cube instance
+    sji : irisreader.sji_cube
+        sji_cube_instance
+
         
     Returns
     -------
-    raster_step : int
-        SJI step
-    diff : float
-        Difference in seconds
+    sji_steps : numpy.array
+        array with closest sji steps
     """
-    
-    # check if data cubes come from the same observation and raise exception if not
-    if raster.primary_headers['STARTOBS'] != sji.primary_headers['STARTOBS']:
-        raise Exception("data cubes are not from the same observation!")
-    
-    diff = np.array( raster.get_timestamps() )-sji.get_timestamps()[sji_step]
-    raster_step = np.argmin( np.abs(diff) )
-    
-    return [raster_step, diff[raster_step]]
-    
+
+    return find_closest_raster( sji, raster )
